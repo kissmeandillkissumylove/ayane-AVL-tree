ab78046c82d31a61d8d59c24363b1e4084e268eb
kissmeandillkissumylove <kissmeandillkissumylove@gmail.com>
2023-10-01 19:41:38 +0700

    add: insert, out;

--- AVL-tree.py
+++ AVL-tree.py
@@ -24,7 +24,7 @@ class Node(object):
 		"""returns node's height. complexity: O(1)."""
 		return self._height
 
-	def _update_height(self) -> None:
+	def update_height(self):
 		"""update height after insertion. complexity: O(1)."""
 		if not self.left and not self.right:
 			self._height = 0
@@ -35,17 +35,11 @@ class Node(object):
 		else:
 			self._height = max(self.left._height, self.right._height) + 1
 
-	def _check_balance(self) -> int:
+	def check_balance(self) -> int:
 		"""checking the balance. if the output is negative, then the tree is unbalanced
 		to the left and vice versa. complexity: O(1)."""
-		if not self.left and not self.right:
-			return 0
-		elif self.right and not self.left:
-			return self.right._height
-		elif self.left and not self.right:
-			return -self.left._height
-		else:
-			return self.right._height - self.left._height
+		return abs(self.left._height if self.left is not None else 0 - (
+			self.right._height if self.right is not None else 0)) >= 2
 
 
 class AVLTree(object):
@@ -74,38 +68,108 @@ class AVLTree(object):
 		if not self._root:  # if no root in the tree
 			self._root = Node(data)
 		else:  # if tree is not empty
-			pass
+			self._root = self._insert(data, self._root)
+
+	def _insert(self, data: Union[int, float, str], node: Node) -> Node:
+		"""helper for insert(). complexity: O(log n)."""
+		if not node:  # no node
+			node = Node(data)
+		elif data < node.data:  # continue in left side
+			node.left = self._insert(data, node.left)
+			if node.check_balance():
+				if data < node.left.data:  # need right rotate for node
+					node = self._right_rotate(node)
+				else:  # need left-right rotate for node
+					node = self._left_right_rotate()
+		elif data > node.data:  # continue in right side
+			node.right = self._insert(data, node.right)
+			if node.check_balance():
+				if data < node.right.data:  # need right-left rotate for node
+					node.right = self._right_left_rotate(node)
+				else:
+					node = self._left_rotate(node)
+		node.update_height()  # last update height for node
+		return node
+
+	def _left_right_rotate(self, node: Node):
+		"""swap nodes positions. complexity: O(1)."""
+		node.left = self._left_rotate(node.right)
+		return self._right_rotate(node)
+
+	def _right_left_rotate(self, node: Node):
+		"""swap nodes positions. complexity: O(1)."""
+		node.right = self._right_rotate(node.right)
+		return self._left_rotate(node)
 
 	@staticmethod
-	def _left_rotate(parent: Node, node: Node) -> None:
-		"""left rotation relative to node. complexity: O(1)."""
-		tmp_x, tmp_y = node, node.right
-		if tmp_x.data < parent.data:
-			parent.left = tmp_y
-			AVLTree._swap_left(tmp_x, tmp_y)
-		elif tmp_x.data > parent.data:
-			parent.right = tmp_y
-			AVLTree._swap_left(tmp_x, tmp_y)
+	def _right_rotate(node: Node) -> Node:
+		"""swap nodes positions. complexity: O(1)."""
+		noder = node  #change positions
+		node = node.left
+		noder.left = node.right
+		node.right = noder
+		node.update_height()  # update heights
+		noder.update_height()
+		return node
 
 	@staticmethod
-	def _right_rotate(parent: Node, node: Node) -> None:
-		"""right rotation relative to node. complexity: O(1)."""
-		tmp_x, tmp_y = node, node.left
-		if tmp_x.data < parent.data:
-			parent.left = tmp_y
-			AVLTree._swap_right(tmp_x, tmp_y)
-		elif tmp_x.data > parent.data:
-			parent.right = tmp_y
-			AVLTree._swap_right(tmp_x, tmp_y)
+	def _left_rotate(node: Node) -> Node:
+		"""swap nodes positions. complexity: O(1)."""
+		nodel = node  #change positions
+		node = node.right
+		nodel.right = node.left
+		node.left = nodel
+		node.update_height()  # update heights
+		nodel.update_height()
+		return node
 
-	@staticmethod
-	def _swap_left(tmp_x: Node, tmp_y: Node) -> None:
-		"""helper for _left_rotation. complexity: O(1)."""
-		tmp_x.right = tmp_y.left
-		tmp_y.left = tmp_x
+	def out(self):
+		"""output a tree in width. complexity: O(n)."""
+		queue = [self._root]
+		while len(queue) > 0:
+			node = queue.pop(0)
+			print("node={}".format(node.data))
+			if node.left is not None:
+				queue.append(node.left)
+			if node.right is not None:
+				queue.append(node.right)
 
-	@staticmethod
-	def _swap_right(tmp_x: Node, tmp_y: Node) -> None:
-		"""helper for _right_rotation. complexity: O(1)."""
-		tmp_x.left = tmp_y.right
-		tmp_y.right = tmp_x
+	def out_graphical(self):
+		"""print tree. complexity: O(n)."""
+		nodes, height = [self._root], 90
+		while nodes:
+			for node in nodes:
+				if node:
+					if node == "S":
+						print(" ", end=" ")
+					else:
+						print(" " * height, node.data, end=" " * height)
+			print("\n")
+			for elt in range(0, len(nodes)):
+				node = nodes.pop(0)
+				if node:
+					if node == "S":
+						nodes.append("S")
+						if len(nodes) == nodes.count(nodes[0]):
+							nodes = []
+							break
+					else:
+						nodes.append(node.left)
+						nodes.append(node.right)
+				else:
+					nodes.append("S")
+					nodes.append("S")
+			height //= 2
+
+
+def main():
+	"""main function"""
+	tree = AVLTree()
+	for _ in range(0, 21):
+		tree.insert(_)
+	tree.out()
+	tree.out_graphical()
+
+
+if __name__ == "__main__":
+	main()
