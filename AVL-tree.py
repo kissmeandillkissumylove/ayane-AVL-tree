da73bafdbde12320fd93635326860c3020f3c784
kissmeandillkissumylove <kissmeandillkissumylove@gmail.com>
2023-10-04 14:33:27 +0700

    AVLTree: fix: insert, out_graphical;;

--- AVL-tree.py
+++ AVL-tree.py
@@ -1,8 +1,8 @@
 """avl-tree implementation.
-09.30.2023. https://github.com/kissmeandillkissumylove"""
+09.30.2023 https://github.com/kissmeandillkissumylove"""
 
 from __future__ import annotations
-from typing import Union
+from typing import Union, NoReturn
 
 
 class Node(object):
@@ -21,20 +21,18 @@ class Node(object):
 		self._height = 0
 
 	def get_height(self) -> int:
-		"""returns node's height. complexity: O(1)."""
+		"""get current node's height. complexity: O(1)."""
 		return self._height
 
-	def update_height(self):
-		"""update height after insertion. complexity: O(1)."""
-		self._height = max(
-			self.left._height if self.left is not None else (-1),
-			self.right._height if self.right is not None else (-1)) + 1
+	def is_balance(self) -> bool:
+		"""check node's balance. complexity: O(1)."""
+		return abs((self.left.get_height() if self.left is not None else -1) - (
+			self.right.get_height() if self.right is not None else -1)) >= 2
 
-	def check_balance(self) -> int:
-		"""checking the balance. if the output is negative, then the tree is unbalanced
-		to the left and vice versa. complexity: O(1)."""
-		return abs(self.left._height if self.left is not None else (-1) - (
-			self.right._height if self.right is not None else (-1))) >= 2
+	def update_height(self) -> None:
+		"""update node's height after inserting. complexity: O(1)."""
+		self._height = max((self.left.get_height() if self.left is not None else -1), (
+			self.right.get_height() if self.right is not None else -1)) + 1
 
 
 class AVLTree(object):
@@ -45,7 +43,7 @@ class AVLTree(object):
 		self._root = None
 
 	def search(self, data: Union[int, float, str]) -> Union[Node, None]:
-		"""method for searching an element in the tree. returns node if node is exists or
+		"""method for searching an element in the tree. returns node if node exists or
 		None if no such node in the tree. complexity: O(log n)."""
 		node = self._root
 		while node:
@@ -57,84 +55,81 @@ class AVLTree(object):
 				node = node.left
 		return
 
-	def insert(self, data: Union[int, float, str]) -> None:
-		"""method for inserting a new element into the tree. returns nothing.
-		complexity: O(log n)."""
-		if not self._root:  # if no root in the tree
+	def insert(self, data: Union[int, float, str]) -> NoReturn:
+		"""insert new element in the tree. complexity: O(log n)."""
+		if self._root is None:
 			self._root = Node(data)
-		else:  # if tree is not empty
+		else:
 			self._root = self._insert(data, self._root)
 
 	def _insert(self, data: Union[int, float, str], node: Node) -> Node:
-		"""helper for insert(). complexity: O(log n)."""
+		"""helper for insert() method. complexity: O(log n)."""
 		if node is None:
 			node = Node(data)
-
-		elif data < node.data:  # left insert
+		elif data < node.data:
 			node.left = self._insert(data, node.left)
-			if node.check_balance():
-				if data < node.left.data:  # LL
+			if node.is_balance():
+				if data < node.left.data:
 					node = self._right_rotate(node)
-				else:  # LR
+				else:
 					node = self._left_right_rotate(node)
-
-		elif data > node.data:  # right insert
+		elif data > node.data:
 			node.right = self._insert(data, node.right)
-			if node.check_balance():
-				if data < node.right.data:  # LR
+			if node.is_balance():
+				if data < node.right.data:
 					node = self._right_left_rotate(node)
-				else:  # RR
+				else:
 					node = self._left_rotate(node)
-
 		node.update_height()
-
 		return node
 
-	def _left_right_rotate(self, node: Node):
-		"""swap nodes positions. complexity: O(1)."""
-		node.left = self._left_rotate(node.left)
-		return self._right_rotate(node)
-
-	def _right_left_rotate(self, node: Node):
-		"""swap nodes positions. complexity: O(1)."""
+	def _right_left_rotate(self, node: Node) -> Node:
+		"""right rotate for node then left rotate for node.right. complexity: O(1)."""
 		node.right = self._right_rotate(node.right)
 		return self._left_rotate(node)
 
-	@staticmethod
-	def _right_rotate(node: Node) -> Node:
-		"""swap nodes positions. complexity: O(1)."""
-		noder = node  #change positions
-		node = node.left
-		noder.left = node.right
-		node.right = noder
-		noder.update_height()  # update heights
-		node.update_height()
-		return node
+	def _left_right_rotate(self, node: Node) -> Node:
+		"""left rotate for node then right rotate for node.right. complexity: O(1)."""
+		node.left = self._left_rotate(node.left)
+		return self._right_rotate(node)
 
 	@staticmethod
 	def _left_rotate(node: Node) -> Node:
-		"""swap nodes positions. complexity: O(1)."""
-		nodel = node  #change positions
+		"""left rotate for node. complexity: O(1)."""
+		node_left = node
 		node = node.right
-		nodel.right = node.left
-		node.left = nodel
-		nodel.update_height()  # update heights
+		node_left.right = node.left
+		node.left = node_left
+		node_left.update_height()
 		node.update_height()
 		return node
 
-	def out(self):
+	@staticmethod
+	def _right_rotate(node: Node) -> Node:
+		"""right rotate for node. complexity: O(1)."""
+		node_right = node
+		node = node.left
+		node_right.left = node.right
+		node.right = node_right
+		node_right.update_height()
+		node.update_height()
+		return node
+
+	def out(self) -> None:
 		"""output a tree in width. complexity: O(n)."""
+		print("{", end=" ")
 		queue = [self._root]
 		while len(queue) > 0:
 			node = queue.pop(0)
-			print("node={}".format(node.data))
+			print(node.data, end=" ")
 			if node.left is not None:
 				queue.append(node.left)
 			if node.right is not None:
 				queue.append(node.right)
+		print("}\n")
 
-	def out_graphical(self):
-		"""print tree. complexity: O(n)."""
+	def out_graphical(self) -> None:
+		"""print tree. complexity: O(n^2)."""
 		nodes, height = [self._root], 90
 		while nodes:
 			for node in nodes:
@@ -143,6 +138,8 @@ class AVLTree(object):
 						print(" ", end=" ")
 					else:
 						print(" " * height, node.data, end=" " * height)
+				else:
+					print(" " * height, " ", end=" " * height)
 			print("\n")
 			for elt in range(0, len(nodes)):
 				node = nodes.pop(0)
